ce66a78af51e11fadce70efb64be6c9f365c3067
mereken <m.nurai.01@mail.ru>
2025-10-16 22:32:12 +0500

    hm4

--- harry-potter-list/src/App.jsx
+++ harry-potter-list/src/App.jsx
@@ -24,16 +24,24 @@ function App() {
       );
       const listData = await listResponse.json();
 
-      if (listData.meals && listData.meals.length >= 2) {
+      if (listData.meals && listData.meals.length >= 6) {
         const seafoodList = listData.meals;
         
-        const [index1, index2] = getRandomIndices(seafoodList.length, 2);
+        const [index1, index2, index3, index4, index5, index6] = getRandomIndices(seafoodList.length, 6);
         const id1 = seafoodList[index1].idMeal;
         const id2 = seafoodList[index2].idMeal;
+        const id3 = seafoodList[index3].idMeal;
+        const id4 = seafoodList[index4].idMeal;
+        const id5 = seafoodList[index5].idMeal;
+        const id6 = seafoodList[index6].idMeal;
 
         const detailUrls = [
           `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id1}`,
-          `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id2}`
+          `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id2}`, 
+          `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id3}`,
+          `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id4}`,
+          `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id5}`,
+          `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id6}`
         ];
 
         const detailResponses = await Promise.all(detailUrls.map(url => fetch(url)));
@@ -42,8 +50,12 @@ function App() {
 
         const meal1 = detailDatas[0].meals[0];
         const meal2 = detailDatas[1].meals[0];
+        const meal3 = detailDatas[2].meals[0];
+        const meal4 = detailDatas[3].meals[0];
+        const meal5 = detailDatas[4].meals[0];
+        const meal6 = detailDatas[5].meals[0];
         
-        setMeals([meal1, meal2]); 
+        setMeals([meal1, meal2, meal3, meal4, meal5, meal6]); 
       }
     } catch (error) {
       console.error("Failed to fetch two random seafood meals:", error);
@@ -57,7 +69,7 @@ function App() {
       <h1>Random Seafood Meals</h1>
       
       <button className="load-btn" onClick={loadTwoRandomSeafoodMeals} disabled={loading}>
-        {loading ? "Loading..." : "Load Two New Meals"}
+        {loading ? "Loading..." : "Load New Meals"}
       </button>
 
       {/* передаем массив из двух блюд в MealList */}
--- harry-potter-list/src/components/MealList.jsx
+++ harry-potter-list/src/components/MealList.jsx
@@ -1,19 +1,38 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 import MealCard from './MealCard';
 import './MealList.css'; 
 
 const MealList = ({ meals }) => {
+  const [searchBar, setSearchBar] = useState('');
   if (meals.length === 0) {
     return <div className="empty-list-message">Click the button to load a random Seafood meal!</div>;
   }
+
+  const filteredMeals = meals.filter((meal => 
+    meal.strMeal.toLowerCase().includes(searchBar.toLowerCase())
+  ));
+
   return (
-    <ul className="meal-list-container single-item">
-      {}
-      {meals.map((meal) => (
-        <MealCard key={meal.idMeal} meal={meal} />
-      ))}
-    </ul>
+    <><div className="search-container">
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchBar}
+        onChange={(e) => setSearchBar(e.target.value)}
+        className="search-input" 
+      />
+      <button onClick={(e) => setSearchBar(e.target.value)} className="clear-btn">Clear</button>
+    
+    </div><ul className="meal-list-container single-item">
+      {filteredMeals.map((meal) => (
+          <MealCard key={meal.idMeal} meal={meal} />
+        ))}
+      </ul></>
   );
 };
+
+
+
 
 export default MealList;
