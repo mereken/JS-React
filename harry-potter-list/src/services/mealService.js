c5ee6c901a57db37630ba697a527786c3396dcd6
mereken <m.nurai.01@mail.ru>
2025-11-13 23:49:08 +0500

    hm6 changed mealService

--- harry-potter-list/src/services/mealService.js
+++ harry-potter-list/src/services/mealService.js
@@ -2,15 +2,11 @@
 const API_BASE = "https://www.themealdb.com/api/json/v1/1";
 
 export const searchMeals = async (query) => {
-  // Case 1: If there is NO search query (Default view)
-  // We fetch the specific "Seafood" category to match your Ocean Basket theme
   if (!query) {
     const response = await fetch(`${API_BASE}/filter.php?c=Seafood`);
     if (!response.ok) throw new Error("Failed to fetch menu");
     const data = await response.json();
     
-    // Note: filter.php returns name, image, and ID, but NOT category/area details.
-    // We manually add the category 'Seafood' so your card displays it nicely.
     const formattedMeals = data.meals.map(meal => ({
       ...meal,
       strCategory: "Seafood" 
@@ -19,8 +15,6 @@ export const searchMeals = async (query) => {
     return formattedMeals || [];
   }
 
-  // Case 2: If the user TYPED something (e.g., "Chicken", "Pie")
-  // We use the search endpoint
   const response = await fetch(`${API_BASE}/search.php?s=${query}`);
   if (!response.ok) throw new Error("Failed to fetch data");
   const data = await response.json();
